7ee525b3f7321ab8bf350a92c6a058ac838ba19d
Tobias Burger <tobias_burger@hotmail.com>
2017-11-08 08:29:55 +0100

    add a simple lisp interpreter (not feature complete!)

---
+++ src/LispInterpreter.elm
@@ -0,0 +1,162 @@
+module ListInterpreter exposing (..)
+
+import LispParser exposing (read, print, Exp(..), Op(..), Value(..), VarName(..))
+import Html exposing (text, ul, li, code, pre)
+import Dict
+
+
+type alias Environment =
+    Dict.Dict String Exp
+
+
+interpretOp : Op -> ( Float -> Float -> Float, Float )
+interpretOp op =
+    case op of
+        Add ->
+            ( (+), 0 )
+
+        Sub ->
+            ( (-), 0 )
+
+        Mul ->
+            ( (*), 1 )
+
+        Div ->
+            ( (/), 1 )
+
+        Max ->
+            ( max, -2147483648 )
+
+        Min ->
+            ( min, 2147483647 )
+
+
+extractValues : List Exp -> List Float
+extractValues exps =
+    exps
+        |> List.map (interpret env)
+        |> List.filterMap
+            (\x ->
+                case x of
+                    Literal (FloatValue v) ->
+                        Just v
+
+                    _ ->
+                        Nothing
+            )
+
+
+floatLit : Float -> Exp
+floatLit =
+    Literal << FloatValue
+
+
+interpret : Environment -> Exp -> Exp
+interpret env exp =
+    case exp of
+        Lambda vars exp ->
+            Debug.crash "not implemented: lambda"
+
+        If cond t f ->
+            let
+                condition =
+                    case interpret env cond of
+                        Literal (FloatValue 0) ->
+                            False
+
+                        _ ->
+                            True
+            in
+                if condition then
+                    interpret env t
+                else
+                    interpret env f
+
+        Set (VarName name) exp ->
+            interpret
+                (Dict.insert name exp env)
+                (floatLit 0)
+
+        Apply exp exps ->
+            let
+                xs =
+                    List.map (interpret env) exps
+            in
+                -- TODO: xs
+                interpret env exp
+
+        OpApply op exps ->
+            let
+                ( opf, base ) =
+                    interpretOp op
+
+                values =
+                    extractValues exps
+
+                value =
+                    List.foldr opf base values
+            in
+                floatLit value
+
+        Literal (FloatValue x) ->
+            floatLit x
+
+        List exps ->
+            List (List.map (interpret env) exps)
+
+        VarRef (VarName name) ->
+            case env |> Dict.get name of
+                Just x ->
+                    interpret env x
+
+                Nothing ->
+                    floatLit 0
+
+
+env : Environment
+env =
+    Dict.fromList [ ( "test", floatLit 8 ) ]
+
+
+samples : List String
+samples =
+    [ "(- 4 3)"
+    , "(if (+ 1 0) 3 45)"
+    , "(if (+ 0 0) 3 45)"
+    , "(min 3 45 0)"
+    , "(max 3 45)"
+    , "(1 2 3 4 5)"
+    , "test"
+    , "(+ (- 1 2) 3 test)"
+    ]
+
+
+main : Html.Html msg
+main =
+    samples
+        |> List.map
+            (\x ->
+                case read x of
+                    Just ast ->
+                        let
+                            result =
+                                interpret env ast
+                        in
+                            li []
+                                [ pre []
+                                    [ text
+                                        (print ast
+                                            ++ "\n"
+                                            ++ toString ast
+                                            ++ "\n"
+                                            ++ toString result
+                                            ++ "\n"
+                                            ++ print result
+                                        )
+                                    ]
+                                ]
+
+                    Nothing ->
+                        text "---"
+            )
+        |> ul []
